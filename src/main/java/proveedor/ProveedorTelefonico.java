56f91e7b3daa70c472b98c19483495bf78444d3e
Michel Quiros Chaves <michelxquiros@gmail.com>
2025-07-01 16:01:46 -0600

    guardar cambios

--- src/main/java/proveedor/ProveedorTelefonico.java
+++ src/main/java/proveedor/ProveedorTelefonico.java
@@ -5,6 +5,5 @@ public class ProveedorTelefonico {
     public static void main (String[] args) {
         ProveedorServer servidor = new ProveedorServer(6000);
         servidor.iniciar(); // arranca el socket
-        System.out.println("hola mundo");
     } 
 }
